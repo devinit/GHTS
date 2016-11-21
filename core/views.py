7e61543fa26db48b574328f6f0d2ef7646b95f8b
Alex Miller <alex.k.miller@gmail.com>
2016-11-21 11:31:16 -0500

    Third times the charm

--- core/views.py
+++ core/views.py
@@ -185,6 +185,7 @@ def adminEdit(request,slug,year):
                 if safeFloat(value)>=0:
                     entry.amount = safeFloat(value)
                     entry.save_reverse()
+        return redirect("core.views.adminEdit",slug=slug,year=year)
     else:
         if Spreadsheet.objects.filter(organisation=organisation,year=year).exists():
             spreadsheet = Spreadsheet.objects.get(organisation=organisation,year=year)
